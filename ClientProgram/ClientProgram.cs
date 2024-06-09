5609d52dd662da43e66f9628cad245d2fc624594
Leonardo Spilere <leonardo.spilere@gmail.com>
2024-06-09 13:44:09 -0300

    initial commit

---
+++ ClientProgram/ClientProgram.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class ClientProgram
+{
+    static Socket _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    const string IP = "192.168.101.7";
+    const int PORT = 8080;
+
+    static void Main(string[] args)
+    {
+        Console.Title = "Client";
+        LoopConnect();
+        SendLoop();
+        Console.ReadLine();
+        _clientSocket.Shutdown(SocketShutdown.Both);
+        _clientSocket.Close();
+    }
+
+    private static void LoopConnect()
+    {
+        int attempts = 0;
+
+        while (!_clientSocket.Connected)
+        {
+            try
+            {
+                attempts++;
+                _clientSocket.Connect(IPAddress.Parse(IP), PORT);
+            }
+            catch (SocketException)
+            {
+                Console.Clear();
+                Console.WriteLine("Connection attempts: " + attempts);
+            }
+        }
+
+        Console.Clear();
+        Console.WriteLine("Connected");
+        Thread receiveThread = new Thread(ReceiveLoop);
+        receiveThread.Start();
+    }
+
+    private static void SendLoop()
+    {
+        while (true)
+        {
+            string message = Console.ReadLine();
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+            _clientSocket.Send(buffer);
+        }
+    }
+
+    private static void ReceiveLoop()
+    {
+        while (true)
+        {
+            byte[] receivedBuffer = new byte[1024];
+            int received = _clientSocket.Receive(receivedBuffer);
+            if (received > 0)
+            {
+                byte[] data = new byte[received];
+                Array.Copy(receivedBuffer, data, received);
+                string text = Encoding.ASCII.GetString(data);
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
---
+++ HostProgram/HostProgram.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+
+class ClientProgram
+{
+    static List<Socket> _clientSockets = new();
+    static Socket _serverSocket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    const string SERVER_IP = "192.168.101.7";
+    const int PORT = 8080;
+
+    static void Main(string[] args)
+    {
+        Console.Title = "Server";
+        SetupServer();
+        Console.ReadLine();
+        CloseAllSockets();
+    }
+
+    private static void SetupServer()
+    {
+        Console.WriteLine("Setting up server...");
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(SERVER_IP), PORT);
+        _serverSocket.Bind(endPoint);
+        _serverSocket.Listen(5);
+        _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        Console.WriteLine("Server setup complete on: " + endPoint);
+    }
+
+    private static void AcceptCallback(IAsyncResult result)
+    {
+        Socket socket = _serverSocket.EndAccept(result);
+        _clientSockets.Add(socket);
+        socket.BeginReceive(new byte[] { 0 }, 0, 0, 0, ReceiveCallback, socket);
+        Console.WriteLine("Client connected");
+        _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+    }
+
+    private static void ReceiveCallback(IAsyncResult result)
+    {
+        Socket current = (Socket)result.AsyncState;
+        int received;
+        try { received = current.EndReceive(result); }
+        catch (SocketException)
+        {
+            Console.WriteLine("Client forcibly disconnected");
+            current.Close();
+            _clientSockets.Remove(current);
+            return;
+        }
+
+        if (received > 0)
+        {
+            byte[] buffer = new byte[received];
+            current.Receive(buffer, buffer.Length, SocketFlags.None);
+            string text = Encoding.ASCII.GetString(buffer);
+            Console.WriteLine("Received Text: " + text);
+
+            string response = $"{current.RemoteEndPoint} - {DateTime.Now}: {text}";
+            byte[] data = Encoding.ASCII.GetBytes(response);
+        
+            foreach (Socket socket in _clientSockets) { socket.Send(data); }
+        }
+
+        current.BeginReceive(new byte[] { 0 }, 0, 0, 0, ReceiveCallback, current);
+    }
+
+    private static void CloseAllSockets()
+    {
+        foreach (Socket socket in _clientSockets)
+        {
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+
+        _serverSocket.Close();
+    }
+}
